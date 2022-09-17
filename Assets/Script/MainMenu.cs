be97525431ba94ab863db88e5f18b338ab35255c
gardeneohr <gardeneo.tp21@mail.mmtc.ac.id>
2022-09-17 14:36:06 +0700

    PemrogramanGame_PilahSampah

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+
+public class MainMenu : MonoBehaviour {
+ 
+   
+    void Start () {
+       
+    }
+   
+    
+    void Update () {
+ 
+        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();     
+            Debug.Log("Quit Game"); 
+        }
+    }
+ 
+ 
+   
+    public void MainGame()
+    {
+        
+           SceneManager.LoadScene("GamePlay");
+    }
+}
